cb0b9cb41a089bef1de605f634d0894bbda53c42
skcapgemini <121160206+skcapgemini@users.noreply.github.com>
2024-06-03 10:38:34 +0100

    New tables added

---
+++ src/Defra.PTS.Sql/Tables/CheckSummary.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE [dbo].[CheckSummary] (
+    Id uniqueidentifier PRIMARY KEY,
+    GBCheck bit,
+    LinkedCheckId uniqueidentifier,
+    CheckerId uniqueidentifier,
+    [Route] int,
+    [Date] date,
+    ScheduledSailingTime time,
+    ApplicationId uniqueidentifier,
+    TravelDocumentId uniqueidentifier,
+    ChipNumber nvarchar(15),
+    OwnerId uniqueidentifier,
+    CheckOutcomeId uniqueidentifier,
+    PTDFailStatusAwaitingApproval bit,
+    PTDFailStatusUnsuccessfulRevokedSuspended bit
+    FOREIGN KEY (CheckOutcomeId) REFERENCES [dbo].[CheckOutcome]([Id]),
+    FOREIGN KEY (OwnerId) REFERENCES [dbo].[Owner]([Id]),
+    FOREIGN KEY ([ApplicationId]) REFERENCES [dbo].[Application]([Id]),
+    FOREIGN KEY ([TravelDocumentId]) REFERENCES [dbo].[TravelDocument]([Id]),
+    FOREIGN KEY ([Route]) REFERENCES [dbo].[Route]([Id]),
+    FOREIGN KEY (CheckerId) REFERENCES [dbo].[Checker]([Id]),
+    FOREIGN KEY (LinkedCheckId) REFERENCES [dbo].[CheckSummary]([Id]),
+    
+);
