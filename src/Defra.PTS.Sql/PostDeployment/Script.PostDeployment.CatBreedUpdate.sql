07a6d3cfc27aa1af6e25094e1d0c0dcf9081143b
Adrian Clare <adrian.clare@capgemini.com>
2024-08-09 14:13:22 +0100

    Script to delete Leopard cat breed

---
+++ src/Defra.PTS.Sql/PostDeployment/Script.PostDeployment.CatBreedUpdate.sql
@@ -0,0 +1,19 @@
+/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+GO
+SET IDENTITY_INSERT [dbo].[Breed] ON 
+GO
+IF EXISTS (SELECT * FROM [dbo].[Breed] Where Id = 81 AND [Name] = 'Leopard Cat (not a domestic breed)')
+BEGIN
+    DELETE from [dbo].[Breed] Where Id in (SELECT Id from [dbo].[Breed] WHERE [Name] = 'Leopard Cat (not a domestic breed)')	
+END
+GO
